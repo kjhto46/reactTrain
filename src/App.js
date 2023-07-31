085b4c67a2f43f28bf5e6a1871ec192913425104
kjhto46 <tony1996kjh@gmail.com>
2023-08-01 01:34:48 +0900

    style-components의 시작

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.h2`color:white;`;
+
+function App() {
+  return (
+    <Father>
+      <BoxOne>
+        <Text>안녕하세요?</Text>
+      </BoxOne>
+      <BoxTwo></BoxTwo>
+    </Father>
+  );
+}
+
+export default App;
