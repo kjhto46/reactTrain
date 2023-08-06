6fe338d108ba0cdc37ae8c15a68258d3e69f05e6
kjhto46 <tony1996kjh@gmail.com>
2023-08-06 17:24:54 +0900

    style-components 활용 애니메이션 scss처럼 작성되는걸 확인해봄

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+
+const Btn = styled.button`
+  color: white;
+  background-color: orange;
+  border: 0;
+  border-radius: 15px;
 `;
-const BoxTwo = styled.div`
+
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
+`;
+const rotateAni = keyframes`
+0% {transform:rotate(0deg);
+border-radius:0px;}
+50% {border-radius:50px;}
+100% {transform:rotate(360deg);
+  border-radius:0px;}
+`;
+
+const Box = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items:center;
   width: 100px;
   height: 100px;
+  background: red;
+  animation: ${rotateAni} 1s linear infinite;
+  span {
+    font-size:36px;
+    &:hover {
+      font-size:40px;
+    }
+  }
 `;
-const Text = styled.h2`color:white;`;
 
 function App() {
   return (
-    <Father>
-      <BoxOne>
-        <Text>안녕하세요?</Text>
-      </BoxOne>
-      <BoxTwo></BoxTwo>
-    </Father>
+    <div>
+      <Father>
+        <Btn>로그인</Btn>
+        <Btn as="a">로그인</Btn>
+        <Input />
+      </Father>
+      <Box>
+        <span>🥰</span>
+        </Box>
+    </div>
   );
 }
 
