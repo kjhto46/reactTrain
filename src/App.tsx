e3728cea7cc11930560cc3488785e73bea9fb838
kjhto46 <tony1996kjh@gmail.com>
2023-08-08 00:27:05 +0900

    typescript를 이용한 차이점 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,12 @@
 import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-color:${(props) => props.theme.textColor};
-background:${(props) => props.theme.backgroundColor};
-`
-
-const Father = styled.div`
-  display: flex;
-`;
-
-const Btn = styled.button`
-  color: white;
-  background-color: orange;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
-`;
-const rotateAni = keyframes`
-0% {transform:rotate(0deg);
-border-radius:0px;}
-50% {border-radius:50px;}
-100% {transform:rotate(360deg);
-  border-radius:0px;}
-`;
-
-const Box = styled.div`
-  display:flex;
-  justify-content: center;
-  align-items:center;
-  width: 100px;
-  height: 100px;
-  background: red;
-  animation: ${rotateAni} 1s linear infinite;
-  span {
-    font-size:36px;
-    &:hover {
-      font-size:40px;
-    }
-  }
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Father>
-        <Btn>로그인</Btn>
-        <Btn as="a">로그인</Btn>
-        <Input />
-      </Father>
-      <Box>
-        <span>🥰</span>
-        </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor='red'/>
+      <Circle bgColor='skyblue'/>
+    </div>
   );
 }
 
