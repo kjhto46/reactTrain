19eb22884976a79b9613578bac718901eea8a280
kjhto46 <tony1996kjh@gmail.com>
2023-08-13 20:27:17 +0900

    TypeScript와 theme구분해서 확인해보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
+import { styled } from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`color:${props => props.theme.textColor}`
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("안녕", value);
+  };
   return (
     <div>
-      <Circle bordercolor="blue" bgcolor='red'/>
-      <Circle bgcolor='skyblue' text="props로 텍스트 있을때"/>
+      <Container>
+        <H1>안녕</H1>
+      </Container>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>로그인</button>
+      </form>
     </div>
   );
 }
