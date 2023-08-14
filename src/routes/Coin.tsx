6c5ca7d60d3a1928326320072a0fffe5de7a974b
kjhto46 <tony1996kjh@gmail.com>
2023-08-14 23:51:15 +0900

    API 사용하기 + TypeScript를 위해 interface를 정의하고 useState<IInterface>지정 작업

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,134 @@
-import { useParams } from "react-router";
-
+import { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
 interface RouteParams {
-  coinId:string;
+  coinId: string;
+}
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const CoinLi = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+interface RouteState {
+  name: string;
+}
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id:string;
+  name:string;
+  symbol:string;
+  rank:number;
+  circulating_supply:number;
+  total_supply:number;
+  max_supply:number;
+  beta_value:number;
+  first_data_at:string;
+  last_updated:string;
+  quotes: {
+    USD : {
+        price:number;
+        volume_24h:number;
+        volume_24h_change_24h:number;
+        market_cap:number;
+        market_cap_change_24h:number;
+        percent_change_15m:number;
+        percent_change_30m:number;
+        percent_change_1h:number;
+        percent_change_6h:number;
+        percent_change_12h:number;
+        percent_change_24h:number;
+        percent_change_7d:number;
+        percent_change_30d:number;
+        percent_change_1y:number;
+        ath_price:number;
+        ath_date:string;
+        percent_from_price_ath:number;
+    }
+  };
 }
 
 function Coin() {
-  const {coinId} = useParams<RouteParams>();
-  return <h1>Coin {coinId}</h1>
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
+  const { state } = useLocation<RouteState>();
+  const [info, setinfo] = useState<IInfoData>();
+  const [priceInfo, setPriceInfo] = useState<IPriceData>();
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setLoading(false);
+      setinfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? "로딩중" : <span>{info?.name}</span>}
+    </Container>
+  );
 }
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -20,9 +21,10 @@ const CoinLi = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -36,49 +38,59 @@ const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
 `;
 
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  }
-];
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coinInfo) => (
-          <CoinLi key={coinInfo.id}>
-            <Link to={`/${coinInfo.id}`}>{coinInfo.name} &rarr;</Link>
-          </CoinLi>
-        ))}
-      </CoinsList>
+      {loading ? (
+        "로딩중"
+      ) : (
+        <CoinsList>
+          {coins.map((coinInfo) => (
+            <CoinLi key={coinInfo.id}>
+              <Link
+                to={{
+                  pathname: `/${coinInfo.id}`,
+                  state: { name: coinInfo.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coinInfo.id}.png`}
+                />
+                {coinInfo.name} &rarr;
+              </Link>
+            </CoinLi>
+          ))}
+        </CoinsList>
+      )}
     </Container>
   );
 }
